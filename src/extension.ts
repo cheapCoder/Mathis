4e2fa93974f569496e7b5debab52ce23e1620bb3
liheng <liheng@shoplazza.com>
2022-09-30 17:35:18 +0800

    fix: no status bar action

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: ExtensionContext) {
 	const hoverDis = languages.registerHoverProvider(config.activeFileLanguage, dispatchHover());
 
 	// StatusBarItem
-	Object.values(barItems).forEach(fn => fn());
+	// Object.values(barItems).forEach(fn => fn());
 	// @ts-ignore Commands
 	context.subscriptions.push(hoverDis, ...Object.keys(commands).map(name => commands[name]));
 }
